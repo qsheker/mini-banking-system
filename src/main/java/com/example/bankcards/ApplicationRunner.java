592a94202c1b70505549f24d354e5f61cf5a95e7
Aldiyar <aldikzhaks@gmail.com>
2025-12-03 22:02:39 +0500

    Init project

--- src/main/java/com/example/bankcards/ApplicationRunner.java
+++ src/main/java/com/example/bankcards/ApplicationRunner.java
@@ -1,4 +1,11 @@
 package com.example.bankcards;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ApplicationRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationRunner.class, args);
+    }
 }
